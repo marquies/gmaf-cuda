1481d111610ddf38b85124790e566d0068d798e2
marquies <me@patrickbreucking.de>
2021-11-06 09:37:20 +0100

    Cleanup of unused code, includes etc.

--- graphcode.cpp
+++ graphcode.cpp
@@ -365,75 +365,9 @@ void calculateSimilarityV(int index, json *gcQuery, std::vector<json> *compares,
         float resultMetrics[3];
         calculateSimilarityThreaded(*gcQuery, compares->at(i), resultMetrics);
 
-        // kernel<<<1,1>>>();
-
-
         std::cout << "Similarity " << resultMetrics[0] << std::endl;
         std::cout << "Recommendation " << resultMetrics[1] << std::endl;
         std::cout << "Inferencing " << resultMetrics[2] << std::endl;
     }
 }
 
-void runFoo() {
-    auto start = std::chrono::system_clock::now();
-    std::vector<json> arr;
-
-
-    //loadGraphCodes((char *) "/Users/breucking/dev/data/GraphCodes/WAPO_CG_Collection", &arr);
-
-    auto loaded = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = loaded - start;
-    std::time_t inter_time = std::chrono::system_clock::to_time_t(loaded);
-
-    //std::cout << "finished computation at " << std::ctime(&inter_time)
-    //          << "elapsed time: " << elapsed_seconds.count() << "s\n";
-
-    std::cout << "loaded " << arr.size() << " graph code files. (" << "elapsed time: " << elapsed_seconds.count() << ")"
-              << std::endl;
-
-
-    int s = 4;
-    int x = arr.size() / s;
-    std::vector<std::thread> threads;
-
-
-    for (int i = 0; i < s; i++) {
-        //std::vector<std::string>   sub(&arr[i*x+1],&arr[(i+1)*x]);
-
-        //calculateSimilarity(i, arr.at(0), arr,  i*x+1, (i+1)*x);
-        //std::thread(calculateSimilarityV, i, arr.at(0), arr, (i*x+1), ((i+1)*x));
-        int end = i == s - 1 ? arr.size() : (i + 1) * x;
-
-        threads.push_back(std::thread(calculateSimilarityV, i, &arr.at(0), &arr, i * x + 1, end));
-//        threads.push_back(std::thread(myThreadFun, i, sub, &tmp_jsons[i]));
-
-    }
-
-    for (auto &th: threads) {
-        th.join();
-    }
-
-    /*
-    //while(true) {
-        for (int i = 1; i < arr.size(); i++) {
-
-            float resultMetrics[3];
-            calculateSimilarity(arr.at(0), arr.at(i), resultMetrics);
-
-            // kernel<<<1,1>>>();
-
-
-            std::cout << "Similarity " << resultMetrics[0] << std::endl;
-            std::cout << "Recommendation " << resultMetrics[1] << std::endl;
-            std::cout << "Inferencing " << resultMetrics[2] << std::endl;
-        }
-   // }*/
-    auto end = std::chrono::system_clock::now();
-
-    elapsed_seconds = end - start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-
-    std::cout << "finished computation at " << std::ctime(&end_time)
-              << "elapsed time: " << elapsed_seconds.count() << "s\n";
-
-}
