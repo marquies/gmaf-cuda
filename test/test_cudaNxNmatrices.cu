ccb16aed545139f56481937b13c3e5a23f065d51
marquies <me@patrickbreucking.de>
2021-11-12 00:05:17 +0100

    Implemented diagonal skip in the check function.

--- test/test_cudaNxNmatrices.cu
+++ test/test_cudaNxNmatrices.cu
@@ -5,6 +5,8 @@
 #include <cuda_runtime.h>
 #include <iostream>
 
+#include <math.h>
+
 #include "../src/graphcode.h"
 #include "../src/cudahelper.cuh"
 #include "../src/helper.h"
@@ -16,18 +18,6 @@
 #define Nrows 4
 #define Ncols 4
 
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-
-
-inline void gpuAssert(cudaError_t code, char *file, int line, bool abort = true)
-{
-    if (code != cudaSuccess)
-    {
-        fprintf(stderr, "GPUassert: %s %s %dn", cudaGetErrorString(code), file, line);
-        if (abort) { exit(code); }
-    }
-}
-
 /*******************/
 /* iDivUp FUNCTION */
 /*******************/
@@ -35,14 +25,22 @@ int iDivUp(int hostPtr, int b){ return ((hostPtr % b) != 0) ? (hostPtr / b + 1) 
 
 
 
-__global__ void check(int *data, unsigned long matrixSize, int *pInt) {
+__global__ void check(int *data, int *comparedata, unsigned long matrixSize, int *pInt) {
     int tid = blockIdx.x;
 
-    //if (tid % matrixSize != 0) {
+     //int q = sqrt((float)matrixSize);
+
+     for (int i = 0; i < matrixSize; i++) {
+        if (tid == i*matrixSize+i) {
+            //Can be used to debug
+            //pInt[tid] = -1;
+            return;
+        }
+     }
+
     if (data[tid] != 0) {
         pInt[tid] = 1;
     }
-    //}
 
 }
 
@@ -86,18 +84,18 @@ int testCudaMatrixMemory()
         }
 
     // --- 2D pitched allocation and host->device memcopy
-    gpuErrchk(cudaMallocPitch(&devPtr, &pitch, Ncols * sizeof(float), Nrows));
-    gpuErrchk(cudaMemcpy2D(devPtr, pitch, hostPtr, Ncols*sizeof(float), Ncols*sizeof(float), Nrows, cudaMemcpyHostToDevice));
+    HANDLE_ERROR(cudaMallocPitch(&devPtr, &pitch, Ncols * sizeof(float), Nrows));
+    HANDLE_ERROR(cudaMemcpy2D(devPtr, pitch, hostPtr, Ncols*sizeof(float), Ncols*sizeof(float), Nrows, cudaMemcpyHostToDevice));
 
     dim3 gridSize(iDivUp(Ncols, BLOCKSIZE_x), iDivUp(Nrows, BLOCKSIZE_y));
     dim3 blockSize(BLOCKSIZE_y, BLOCKSIZE_x);
 
     test_kernel_2D<<<gridSize, blockSize>>>(devPtr, pitch);
 
-    gpuErrchk(cudaPeekAtLastError());
-    gpuErrchk(cudaDeviceSynchronize());
+    HANDLE_ERROR(cudaPeekAtLastError());
+    HANDLE_ERROR(cudaDeviceSynchronize());
 
-    gpuErrchk(cudaMemcpy2D(hostPtr, Ncols * sizeof(float), devPtr, pitch, Ncols * sizeof(float), Nrows, cudaMemcpyDeviceToHost));
+    HANDLE_ERROR(cudaMemcpy2D(hostPtr, Ncols * sizeof(float), devPtr, pitch, Ncols * sizeof(float), Nrows, cudaMemcpyDeviceToHost));
 
     for (int i = 0; i < Nrows; i++)
         for (int j = 0; j < Ncols; j++)
@@ -116,22 +114,22 @@ void testCudaLinearMatrixMemory(){
     int matrix1[gc1Dictionary.size()][gc1Dictionary.size()];
     convertDict2Matrix(gc1Dictionary.size(), (int *) matrix1, gcq["matrix"]);
 
-    int a[gcq.size() * gcq.size()];
+    int inputMatrix[gcq.size() * gcq.size()];
     int count = 0;
     for (int i = 0; i < gcq.size(); i++)
         for (int j = 0; j < gcq.size(); j++) {
-            a[count++] = matrix1[i][j];
+            inputMatrix[count++] = matrix1[i][j];
         }
 
-    int *d_a;
+    int *gpu_inputMatrix;
     int *founds;
-    // Allocate device memory for a
-    //cudaMalloc((void**)&d_a, sizeof(int) );
+    // Allocate device memory for inputMatrix
+    //cudaMalloc((void**)&gpu_inputMatrix, sizeof(int) );
 
     int items = 4;
 
 
-    HANDLE_ERROR(cudaMalloc((void**)&d_a, sizeof(int) * items) );
+    HANDLE_ERROR(cudaMalloc((void**)&gpu_inputMatrix, sizeof(int) * items) );
     HANDLE_ERROR(cudaMalloc((void**)&founds, sizeof(int) * items) );
     /*
     cudaMemcpy2DToArray (dst,
@@ -146,10 +144,10 @@ void testCudaLinearMatrixMemory(){
     */
 
     // Transfer data from host to device memory
-    HANDLE_ERROR(cudaMemcpy(d_a, a, sizeof(int) * gcq.size() * gcq.size(), cudaMemcpyHostToDevice));
+    HANDLE_ERROR(cudaMemcpy(gpu_inputMatrix, inputMatrix, sizeof(int) * gcq.size() * gcq.size(), cudaMemcpyHostToDevice));
 
 
-    check<<<items, 1>>>(d_a, gcq.size(), founds);
+    check<<<items, items>>>(gpu_inputMatrix, gpu_inputMatrix, gcq.size(), founds);
 
 
     int f[items];
@@ -159,12 +157,19 @@ void testCudaLinearMatrixMemory(){
         std::cout << "pos: " << i << " value: " << f[i] << std::endl;
     }
 
-    HANDLE_ERROR(cudaFree(d_a));
+    HANDLE_ERROR(cudaFree(gpu_inputMatrix));
 }
 
 
 int main(int, char**)
 {
+    int q = sqrt((float)4);
+
+    for (int i = 0; i < q; i++) {
+
+            std::cout <<i*q+i << std::endl;
+    }
+
     testCudaMatrixMemory();
     testCudaLinearMatrixMemory();
 
