3774f929fa94d4fb9d0e6c137a9c001bd008d99d
marquies <me@patrickbreucking.de>
2021-12-27 13:39:40 +0100

    Added a test to compare scpu/thread/cuda implementations of simple GC comparison.

--- test/test_cpuNxNmatrices.cpp
+++ test/test_cpuNxNmatrices.cpp
@@ -7,19 +7,28 @@
 #include <chrono>
 #include <ctime>
 
-/********/
-/* MAIN */
-void testCpuCalc();
+#include "../src/helper.h"
 
-/********/
+void testCpuSeqCalc();
+void testCpuThreadCalc();
+int calculateSimilaritySequentialOrdered(json gc1, json gc2, float *results);
+
+/**
+ * MAIN
+ */
 int main(int, char**)
 {
-    testCpuCalc();
-   std::cout << "foo";
+
+    testCpuSeqCalc();
+    testCpuThreadCalc();
 
 }
 
-void testCpuCalc() {
+void testCpuSeqCalc() {
+
+    // 1. Create n gc matrices of size l
+    // 2. Calc metrix for each GC
+
 
     int n = 10000;
     nlohmann::json gcq;
@@ -40,7 +49,16 @@ void testCpuCalc() {
 
     auto start = std::chrono::system_clock::now();
 
-    const std::vector<Metrics> &metrics = gc.calculateSimilarityV(0, &gcq, &others, 0, n);
+    float results[3];
+
+    // Do a plain simple version of the calc
+    for (const auto agc: others) {
+        calculateSimilaritySequentialOrdered(gcq, agc, results);
+    }
+
+    // Do a plain simple version of the calc
+
+
 
     auto end = std::chrono::system_clock::now();
 
@@ -49,5 +67,126 @@ void testCpuCalc() {
 
     std::cout << "finished computation at " << std::ctime(&end_time)
               << "elapsed time: " << elapsed_seconds.count() << "s\n";
+
+}
+
+void testCpuThreadCalc() {
+
+    // 1. Create n gc matrices of size l
+    // 2. Calc metrix for each GC
+
+    int n = 10000;
+    nlohmann::json gcq;
+    gcq["dictionary"] = { "head", "body", "foot"};
+    gcq["matrix"] = {{1,1,0}, {0,1,0}, {0,1,1}};
+
+    nlohmann::json gce;
+    gce["dictionary"] = { "head", "body", "torso"};
+    gce["matrix"] = {{1,2,0}, {0,1,0}, {0,0,1}};
+
+    std::vector<json> others;
+
+    for(int i = 0; i < n; i++) {
+        others.push_back(gce);
+    }
+
+    gmaf::GraphCode gc;
+
+    auto start = std::chrono::system_clock::now();
+
+
+
+
+
+    auto end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s\n";
+
+}
+
+
+
+int calculateSimilaritySequentialOrdered(json gc1, json gc2, float *results) {
+    json gc1Dictionary = gc1["dictionary"];
+    json gc2Dictionary = gc2["dictionary"];
+
+    std::string gc1Dict[gc1Dictionary.size()];
+
+    int n = 0;
+
+    int sim = 0;
+
+
+
+    int matrix1[gc1Dictionary.size()][gc1Dictionary.size()];
+    convertDict2Matrix(gc1Dictionary.size(), (int *) matrix1, gc1["matrix"]);
+
+    int matrix2[gc2Dictionary.size()][gc2Dictionary.size()];
+    convertDict2Matrix(gc2Dictionary.size(), (int *) matrix2, gc2["matrix"]);
+
+
+    std::vector<std::string> dict2;
+    for (const auto &item2: gc2Dictionary.items()) {
+        dict2.push_back(item2.value().get<std::string>());
+    }
+
+
+    for (const auto &item: gc1Dictionary.items()) {
+        //std::cout << item.value() << "\n";
+        std::string str = item.value().get<std::string>();
+        gc1Dict[n++] = str;
+
+
+        for (const auto &item2: gc2Dictionary.items()) {
+            if (str == item2.value()) {
+                //std::cout << "Match" << std::endl;
+                sim++;
+            }
+        }
+    }
+    int num_of_non_zero_edges = 0;
+    int edge_metric_count = 0;
+    int edge_type = 0;
+    for (int i = 0; i < gc1Dictionary.size(); i++) {
+        for (int j = 0; j < gc1Dictionary.size(); j++) {
+
+            if (i != j && matrix1[i][j] != 0) {
+                num_of_non_zero_edges++;
+
+                int position1 = i;
+                int position2 = j;
+                //std::cout << "Pos " << position1 << " " << position2 << std::endl;
+                if (position1 == -1 || position2 == -1) {
+                    continue;
+                }
+
+                int edge = matrix2[position1][position2];
+                if (edge != 0) {
+                    edge_metric_count++;
+                }
+                if (edge == matrix1[i][j]) {
+                    edge_type++;
+                }
+
+            }
+        }
+    }
+
+    float node_metric = (float) sim / (float) gc1Dictionary.size();
+    float edge_metric = 0.0;
+    if (num_of_non_zero_edges > 0)
+        edge_metric = (float) edge_metric_count / (float) num_of_non_zero_edges;
+    float edge_type_metric = 0.0;
+    if (edge_metric_count > 0)
+        edge_type_metric = (float) edge_type / (float) edge_metric_count;
+
+    results[0] = node_metric;
+    results[1] = edge_metric;
+    results[2] = edge_type_metric;
+    return 0;
 
 }
